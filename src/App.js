dd8f50db7bd68733737a164834d16786d6f899cf
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-06-15 15:01:38 +0100

    feat: home header & feature

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Wrapper, Header, Feature } from './components';
 import './App.css';
+import logo from './logo.svg';
 
-function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
-}
+export function App() {
+        return (
+   
+    <Wrapper>
+      <Header>
+        <Header.Image src={logo} alt="Netflix" />
+        <Header.Button>Sign In</Header.Button>
+      </Header>
 
-export default App;
+      <Feature>
+        <Feature.Title>Unlimited films, TV programmes and more.</Feature.Title>
+        <Feature.SubTitle>Watch anywhere. Cancel at any time.</Feature.SubTitle>
+        <Feature.Form>
+          <Feature.Input placeholder="Email address" />
+          <Feature.Button>Try it now</Feature.Button>
+        </Feature.Form>
+        <Feature.Text>
+          Ready to watch? Enter your email to create or restart your membership.
+        </Feature.Text>
+      </Feature>
+    </Wrapper>
+        );
+    }
