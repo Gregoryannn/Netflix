415cb35280c267d463a379a69e34a09606a621d2
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-06-22 15:39:54 +0100

    feat: context feature

--- src/components/jumbotron/jumbotron.js
+++ src/components/jumbotron/jumbotron.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import { Container, Inner, Pane, Title, SubTitle, Image } from './styles/Jumbotron';
+import styled from 'styled-components';
+export const Container = styled.div`
+  display: flex;
+  border-bottom: 8px solid #222;
+  padding: 50px 5%;
+  color: white;
+`;
+export const Inner = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: ${(props) => props.direction};
+  max-width: 1100px;
+  margin: auto;
+  width: 100%;
+`;
+export const Pane = styled.div`
+  width: 50%;
+`;
 
-export function Jumbotron({ children, direction = 'row', ...restProps }) {
-    return (
-        <Container {...restProps}>
-            <Inner direction={direction}>{children}</Inner>
-        </Container>
-    );
-}
+export const Title = styled.h1`
+  font-size: 50px;
+  line-height: 1.1;
+  margin-bottom: 8px;
+`;
 
-Jumbotron.Pane = function JumbotronPane({ children, ...restProps }) {
-    return <Pane {...restProps}>{children}</Pane>;
-};
-
-Jumbotron.Title = function JumbotronTitle({ children, ...restProps }) {
-    return <Title {...restProps}>{children}</Title>;
-};
-
-Jumbotron.SubTitle = function JumbotronSubTitle({ children, ...restProps }) {
-    return <SubTitle {...restProps}>{children}</SubTitle>;
-};
-
-Jumbotron.Image = function JumbotronImage({ ...restProps }) {
-    return <Image {...restProps} />;
-};
+export const SubTitle = styled.h2`
+  font-size: 26px;
+  font-weight: normal;
+  line-height: normal;
+`;
+export const Image = styled.img`
+  height: 398px;
+`;
