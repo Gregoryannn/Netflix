34669a52060c5be8785f4da7b427a668bb820794
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-06-27 10:06:11 +0100

    refactor: updates for scrimba

--- src/components/card/styles/Card.js
+++ src/components/card/styles/Card.js
@@ -1,19 +1,5 @@
 import styled from 'styled-components/macro';
-export const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 50px;
-  box-sizing: border-box;
-  &:last-of-type {
-    margin-bottom: 0;
-  }
-`;
-export const Group = styled.div`
-  display: flex;
-  flex-direction: ${({ flexDirection }) => (flexDirection === 'row' ? 'row' : 'column')};
-  ${({ alignItems }) => alignItems && `align-items: ${alignItems}`};
-  ${({ margin }) => margin && `margin: ${margin}`};
-`;
+
 export const Title = styled.p`
   font-size: 24px;
   color: #e5e5e5;
@@ -22,6 +8,34 @@ export const Title = styled.p`
   margin-right: 56px;
   margin-top: 0;
 `;
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 50px;
+  box-sizing: border-box;
+  > ${Title} {
+    @media (max-width: 1000px) {
+      margin-left: 30px;
+    }
+  }
+  &:last-of-type {
+    margin-bottom: 0;
+  }
+`;
+
+export const Group = styled.div`
+  display: flex;
+  flex-direction: ${({ flexDirection }) => (flexDirection === 'row' ? 'row' : 'column')};
+  ${({ alignItems }) => alignItems && `align-items: ${alignItems}`};
+  ${({ margin }) => margin && `margin: ${margin}`};
+ 
+  > ${Container}:first-of-type {
+    @media (min-width: 1100px) {
+      margin-top: -150px;
+    }
+  }
+`;
+
 export const SubTitle = styled.p`
   font-size: 12px;
   color: #fff;
@@ -71,16 +85,30 @@ export const Item = styled.div`
     transform: scale(1.3);
     z-index: 99;
   }
-  &:hover ${Meta}, &:hover ${Text}, &:hover ${SubTitle} {
-    display: block;
-    z-index: 100;
+  @media (min-width: 1200px) {
+    &:hover ${Meta}, &:hover ${Text}, &:hover ${SubTitle} {
+      display: block;
+      z-index: 100;
+    }
   }
   &:first-of-type {
     margin-left: 56px;
+    @media (max-width: 1000px) {
+      margin-left: 30px;
+    }
   }
   &:last-of-type {
     margin-right: 56px;
+    @media (max-width: 1000px) {
+      margin-right: 30px;
+    }
   }
+`;
+export const FeatureText = styled.p`
+  font-size: 18px;
+  color: white;
+  font-weight: ${({ fontWeight }) => (fontWeight === 'bold' ? 'bold' : 'normal')};
+  margin: 0;
 `;
 export const Feature = styled.div`
   display: flex;
@@ -92,15 +120,21 @@ export const Feature = styled.div`
   background-position-x: right;
   background-repeat: no-repeat;
   background-color: black;
+  @media (max-width: 1000px) {
+    height: auto;
+    background-size: auto;
+    ${Title} {
+      font-size: 20px;
+      line-height: 20px;
+      margin-bottom: 10px;
+    }
+    ${FeatureText} {
+      font-size: 14px;
+    }
+  }
 `;
 export const FeatureTitle = styled(Title)`
   margin-left: 0;
-`;
-export const FeatureText = styled.p`
-  font-size: 18px;
-  color: white;
-  font-weight: ${({ fontWeight }) => (fontWeight === 'bold' ? 'bold' : 'normal')};
-  margin: 0;
 `;
 export const Content = styled.div`
   margin: 56px;
@@ -113,8 +147,11 @@ export const Content = styled.div`
     top: 20px;
     cursor: pointer;
   }
+  @media (max-width: 1000px) {
+    margin: 30px;
+    max-width: none;
+  }
 `;
-
 export const Maturity = styled.div`
   background-color: ${({ rating }) => (rating >= 15 ? 'red' : 'green')};
   border-radius: 15px;
