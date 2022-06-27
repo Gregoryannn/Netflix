680c8a921f14a73cb41483126502da43484ca568
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-06-27 10:32:47 +0100

    tests: adding component coverage

--- src/components/player/index.js
+++ src/components/player/index.js
@@ -1,9 +1,7 @@
 import React, { useState, useContext, createContext } from 'react';
 import ReactDOM from 'react-dom';
 import { Container, Button, Overlay, Inner, Close } from './styles/player';
-
 export const PlayerContext = createContext();
-
 export default function Player({ children, ...restProps }) {
     const [showPlayer, setShowPlayer] = useState(false);
     return (
@@ -12,23 +10,25 @@ export default function Player({ children, ...restProps }) {
         </PlayerContext.Provider>
     );
 }
-Player.Video = function PlayerVideo({ ...restProps }) {
-    const { showPlayer, setShowPlayer } = useContext(PlayerContext);
-    return showPlayer
-        ? ReactDOM.createPortal(
-            <Overlay onClick={() => setShowPlayer(false)}>
-                <Inner>
-                    <video id="netflix-player" controls>
-                        <source src="/videos/bunny.mp4" type="video/mp4" />
-                    </video>
-                    <Close />
-                </Inner>
-            </Overlay>,
-            document.body
-        )
-        : null;
+
+Player.Video = function PlayerVideo({ src, ...restProps }) {
+        const { showPlayer, setShowPlayer } = useContext(PlayerContext);
+
+        return showPlayer
+            ? ReactDOM.createPortal(
+                    <Overlay onClick={() => setShowPlayer(false)} data-testid="player">
+                        <Inner>
+                            <video id="netflix-player" controls>
+                                <source src={src} type="video/mp4" />
+                            </video>
+                            <Close />
+                        </Inner>
+                    </Overlay>,
+                    document.body
+                    )
+                    : null;
 };
-Player.Button = function PlayerButton({ ...restProps }) {
-    const { showPlayer, setShowPlayer } = useContext(PlayerContext);
-    return <Button onClick={() => setShowPlayer(!showPlayer)}>Play</Button>;
+                    Player.Button = function PlayerButton({...restProps}) {
+  const {showPlayer, setShowPlayer} = useContext(PlayerContext);
+                    return <Button onClick={() => setShowPlayer(!showPlayer)}>Play</Button>;
 };
